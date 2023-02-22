c8621cbfa074fc36b0d83d2bff5f82007d95e3f1
jv88899 <jv88899@gmail.com>
2023-02-21 19:00:54 -0600

    created basic landing page

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,9 @@
-import useWindowSize from "@/utils/useWindowSize";
 import Head from "next/head";
+import Image from "next/image";
 import homeStyles from "../styles/Home.module.css";
-
-function SmallScreen() {
-  return <h1>amll</h1>;
-}
-
-function LargeScreen() {
-  return <h1>Large</h1>;
-}
+import Link from "next/link";
 
 export default function Home() {
-  const { width } = useWindowSize();
-
-  let body = <p style={{ marginTop: "80px" }}>Loading...</p>;
-  if (width < 768) {
-    body = <SmallScreen />;
-  } else if (width >= 768) {
-    body = <LargeScreen />;
-  }
-
   return (
     <>
       <Head>
@@ -28,7 +12,47 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {<div className={homeStyles.wrapper}>{body}</div>}
+      {
+        <div className={homeStyles.wrapper}>
+          <section className={homeStyles.splashScreenWrapper}>
+            <div className={homeStyles.innerWrapper}>
+              <Image
+                src="/../public/media/icons/music_note_small.png"
+                height={100}
+                width={100}
+                alt="3d music note"
+                className={homeStyles.musicNoteSmallIcon}
+              />
+              <div className={homeStyles.aboutWrapper}>
+                <h1 className={homeStyles.title}>A musical adventure.</h1>
+                <div className={homeStyles.description}>
+                  Join us as we explore music and discover the greatest albums
+                  ever made.
+                </div>
+              </div>
+              <div className={homeStyles.buttonWrapper}>
+                <Link href="/" className={homeStyles.moreInformationButton}>
+                  What is this place?
+                </Link>
+                <Link href="/signup" className={homeStyles.signUpButton}>
+                  I&apos;ve heard enough, I&apos;m in!
+                </Link>
+              </div>
+            </div>
+          </section>
+          <section className={homeStyles.aboutScreenWrapper}>
+            <h2 className={homeStyles.aboutTitle}>What is this place?</h2>
+            <p className={homeStyles.aboutDescription}>
+              My Still Untitled Music Project is a collection of albums. But not
+              just any albums. These albums are hand-selected every week.
+            </p>
+            <p className={homeStyles.aboutDescription}>
+              We&apos;ve curated the list. Now all you have to do is vote.
+            </p>
+            <Link href="/signup">Sign up for free</Link>
+          </section>
+        </div>
+      }
     </>
   );
 }
