c822e90b9e80727f6786c30c3c6d65d80f3a56f6
jv88899 <jv88899@gmail.com>
2023-03-03 20:46:05 -0600

    removed an accidental and unused dayjs statement

--- pages/vote.js
+++ pages/vote.js
@@ -7,7 +7,6 @@ import Link from "next/link";
 import React from "react";
 import toast from "react-hot-toast";
 import styles from "../styles/Vote.module.css";
-dayjs;
 
 const successfulUpvote = () => toast.success("Upvoted");
 const successfulDownvote = () => toast.success("Downvoted");
