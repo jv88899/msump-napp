8e06d64299d0c862646adcd97d33d531a1bbfebc
jv88899 <jv88899@gmail.com>
2023-03-03 20:41:51 -0600

    created Layout component

--- pages/vote.js
+++ pages/vote.js
@@ -1,4 +1,5 @@
 import Album from "@/components/Album";
+import Layout from "@/components/Layout";
 import { supabase } from "@/utils/supabase";
 import { useQuery } from "@tanstack/react-query";
 import dayjs from "dayjs";
@@ -151,105 +152,109 @@ export default function Vote() {
   }
 
   return (
-    <div className={styles.wrapper}>
-      <h2 className={styles.pageTitle}>Vote</h2>
-      {error ? <span>Error {error.message}</span> : null}
+    <Layout>
+      <div className={styles.wrapper}>
+        <h2 className={styles.pageTitle}>Vote</h2>
+        {error ? <span>Error {error.message}</span> : null}
 
-      {isLoading ? (
-        <span>Loading...</span>
-      ) : (
-        <>
-          {albumIndex < numberOfAlbums && !allVotesUsed ? (
-            <>
-              <div className={styles.albumContainer}>
-                {currentAlbum && (
-                  <Album
-                    album={currentAlbum}
-                    upvote={upvote}
-                    downvote={downvote}
-                    handleUpdateCurrentAlbum={handleUpdateCurrentAlbum}
-                  />
-                )}
-                <div className={styles.albumInfoWrapper}>
-                  <div className={styles.albumVotingInformationWrapper}>
-                    <span className={styles.upvotesContainer}>+ {upvotes}</span>
-                    <span className={styles.downvotesContainer}>
-                      - {downvotes}
-                    </span>
+        {isLoading ? (
+          <span>Loading...</span>
+        ) : (
+          <>
+            {albumIndex < numberOfAlbums && !allVotesUsed ? (
+              <>
+                <div className={styles.albumContainer}>
+                  {currentAlbum && (
+                    <Album
+                      album={currentAlbum}
+                      upvote={upvote}
+                      downvote={downvote}
+                      handleUpdateCurrentAlbum={handleUpdateCurrentAlbum}
+                    />
+                  )}
+                  <div className={styles.albumInfoWrapper}>
+                    <div className={styles.albumVotingInformationWrapper}>
+                      <span className={styles.upvotesContainer}>
+                        + {upvotes}
+                      </span>
+                      <span className={styles.downvotesContainer}>
+                        - {downvotes}
+                      </span>
+                    </div>
+                    <div className={styles.albumCommentsWrapper}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className={styles.albumCommentIcon}
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                        onClick={showComments}
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"
+                        />
+                      </svg>
+                      <div className={styles.albumCommentCountContainer}>
+                        {comments && comments.length}
+                      </div>
+                    </div>
                   </div>
-                  <div className={styles.albumCommentsWrapper}>
+                </div>
+                <div className={styles.albumTrackListWrapper}>
+                  <button
+                    className={styles.trackListButton}
+                    onClick={showTrackList}
+                  >
+                    View Track List
+                  </button>
+                </div>
+                <div className={styles.buttonWrapper}>
+                  <button onClick={handleDownvote}>
                     <svg
+                      className={styles.thumbsDownIcon}
                       xmlns="http://www.w3.org/2000/svg"
-                      className={styles.albumCommentIcon}
                       fill="none"
                       viewBox="0 0 24 24"
                       stroke="currentColor"
                       strokeWidth={2}
-                      onClick={showComments}
                     >
                       <path
                         strokeLinecap="round"
                         strokeLinejoin="round"
-                        d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"
+                        d="M10 14H5.236a2 2 0 01-1.789-2.894l3.5-7A2 2 0 018.736 3h4.018a2 2 0 01.485.06l3.76.94m-7 10v5a2 2 0 002 2h.096c.5 0 .905-.405.905-.904 0-.715.211-1.413.608-2.008L17 13V4m-7 10h2m5-10h2a2 2 0 012 2v6a2 2 0 01-2 2h-2.5"
                       />
                     </svg>
-                    <div className={styles.albumCommentCountContainer}>
-                      {comments && comments.length}
-                    </div>
-                  </div>
+                  </button>
+                  <button onClick={handleUpvote}>
+                    <svg
+                      className={styles.thumbsUpIcon}
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5"
+                      />
+                    </svg>
+                  </button>
                 </div>
-              </div>
-              <div className={styles.albumTrackListWrapper}>
-                <button
-                  className={styles.trackListButton}
-                  onClick={showTrackList}
-                >
-                  View Track List
-                </button>
-              </div>
-              <div className={styles.buttonWrapper}>
-                <button onClick={handleDownvote}>
-                  <svg
-                    className={styles.thumbsDownIcon}
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M10 14H5.236a2 2 0 01-1.789-2.894l3.5-7A2 2 0 018.736 3h4.018a2 2 0 01.485.06l3.76.94m-7 10v5a2 2 0 002 2h.096c.5 0 .905-.405.905-.904 0-.715.211-1.413.608-2.008L17 13V4m-7 10h2m5-10h2a2 2 0 012 2v6a2 2 0 01-2 2h-2.5"
-                    />
-                  </svg>
-                </button>
-                <button onClick={handleUpvote}>
-                  <svg
-                    className={styles.thumbsUpIcon}
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5"
-                    />
-                  </svg>
-                </button>
-              </div>
-            </>
-          ) : (
-            <>
-              <span>You have used all of your votes for today</span>
-              <Link href="/dashboard">Return to Dashboard</Link>
-            </>
-          )}
-        </>
-      )}
-    </div>
+              </>
+            ) : (
+              <>
+                <span>You have used all of your votes for today</span>
+                <Link href="/dashboard">Return to Dashboard</Link>
+              </>
+            )}
+          </>
+        )}
+      </div>
+    </Layout>
   );
 }
